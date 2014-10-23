e884c54ac3fe5b801e96a2e6ccf6a0e948d02ce4
Rafal Gancarz <rafal.gancarz@opencredo.com>
2014-10-23 12:22:11 +0100

    Created a suite of integration tests.

--- src/main/java/example/domain/Event.java
+++ src/main/java/example/domain/Event.java
@@ -14,13 +14,16 @@ import java.util.UUID;
 public class Event {
 
     @PrimaryKeyColumn(name = "id", ordinal = 2, type = PrimaryKeyType.CLUSTERED, ordering = Ordering.DESCENDING)
-    private UUID id;
+    private final UUID id;
+
     @PrimaryKeyColumn(name = "type", ordinal = 0, type = PrimaryKeyType.PARTITIONED)
-    private String type;
+    private final String type;
+
     @PrimaryKeyColumn(name = "bucket", ordinal = 1, type = PrimaryKeyType.PARTITIONED)
-    private String bucket;
+    private final String bucket;
+
     @Column
-    private Set<String> tags = new HashSet<String>();
+    private final Set<String> tags = new HashSet<>();
 
     public Event(UUID id, String type, String bucket, Set<String> tags) {
         this.id = id;
@@ -48,5 +51,29 @@ public class Event {
     @Override
     public String toString() {
         return "Event{" + "id=" + id + ", type=" + type + ", bucket=" + bucket + ", tags=" + tags + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Event event = (Event) o;
+
+        if (!bucket.equals(event.bucket)) return false;
+        if (!id.equals(event.id)) return false;
+        if (!tags.equals(event.tags)) return false;
+        if (!type.equals(event.type)) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + type.hashCode();
+        result = 31 * result + bucket.hashCode();
+        result = 31 * result + tags.hashCode();
+        return result;
     }
 }
---
+++ src/test/java/example/CassandraTemplateIntegrationTest.java
@@ -0,0 +1,42 @@
+package example;
+
+import com.datastax.driver.core.querybuilder.QueryBuilder;
+import com.datastax.driver.core.querybuilder.Select;
+import com.datastax.driver.core.utils.UUIDs;
+import com.google.common.collect.ImmutableSet;
+import example.domain.Event;
+import org.hamcrest.core.IsEqual;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.cassandra.core.CassandraOperations;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CassandraTemplateIntegrationTest extends BaseIntegrationTest {
+
+    public static final String TIME_BUCKET = "2014-01-01";
+
+    @Autowired
+    private CassandraOperations cassandraTemplate;
+
+    @Test
+    public void supportsPojoToCqlMappings() {
+        Event event = new Event(UUIDs.timeBased(), "type1", TIME_BUCKET, ImmutableSet.of("tag1", "tag3"));
+        cassandraTemplate.insert(event);
+
+        Select select = QueryBuilder.select().from("event").where(QueryBuilder.eq("type", "type1")).and(QueryBuilder.eq("bucket", TIME_BUCKET)).limit(10);
+
+        Event retrievedEvent = cassandraTemplate.selectOne(select, Event.class);
+
+        assertThat(retrievedEvent, IsEqual.equalTo(event));
+
+        List<Event> retrievedEvents = cassandraTemplate.select(select, Event.class);
+
+        assertThat(retrievedEvents.size(), is(1));
+        assertThat(retrievedEvents, hasItem(event));
+    }
+}
